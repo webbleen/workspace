e4c3e3f380adb6d6f5421275f2f018d9dfb3a9b8
webbleen <webbleen@gmail.com>
2019-07-10 20:51:17 +0900

    StudyWPF
    StudyWPF

---
+++ DoNet/WPF/StudyWPF/HappyWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace HappyWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Human human = this.FindResource("human") as Human;
+            if (human != null)
+            {
+                //MessageBox.Show(human.Name);
+                MessageBox.Show(human.Child.Name);
+            }
+        }
+    }
+
+    [TypeConverterAttribute(typeof(Name2HumanTypeConverter))]
+    public class Human
+    {
+        public string Name { get; set; }
+        public Human Child { get; set; }
+    }
+
+    public class Name2HumanTypeConverter : TypeConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            string name = value.ToString();
+            Human child = new Human();
+            child.Name = name;
+            return child;
+        }
+    }
+}
